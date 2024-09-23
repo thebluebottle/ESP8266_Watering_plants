782817c953b77b97a6a347c69263f47479e0a403
Brandon Alejandro Mendoza Medina <brandonalejandromendozamedina@Brandons-MacBook-Pro.local>
2024-09-22 18:53:00 -0600

    new swift approach

---
+++ watering_app/WateringApp/WateringApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WateringApp
+//
+//  Created by Brandon Alejandro Mendoza Medina on 22/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ watering_app/WateringApp/WateringAppTests/WateringAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WateringAppTests.swift
+//  WateringAppTests
+//
+//  Created by Brandon Alejandro Mendoza Medina on 22/09/24.
+//
+
+import Testing
+@testable import WateringApp
+
+struct WateringAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
